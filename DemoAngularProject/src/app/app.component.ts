43076f8c48ced7f4021377a4693548c52fbcc3f5
skpmcakaushiksoni-dotcom <skpmcakaushiksoni@gmail.com>
2026-01-21 18:03:07 +0530

    Student data page added

--- DemoAngularProject/src/app/app.component.ts
+++ DemoAngularProject/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { StudentRegistartionComponent } from './student-registartion/student-registartion.component';
 import { LoginComponent } from './login/login.component';
+import { ManageStudentComponent } from './manage-student/manage-student.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [StudentRegistartionComponent,LoginComponent],
+  imports: [StudentRegistartionComponent,LoginComponent,ManageStudentComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ DemoAngularProject/src/app/manage-student/manage-student.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-student',
+  imports: [],
+  templateUrl: './manage-student.component.html',
+})
+export class ManageStudentComponent {
+
+}
